252c787b2d2d97bd4005b637b013c34b90a831a8
chn04 <79628038+chn04@users.noreply.github.com>
2022-03-08 11:59:10 +0800

    Add files via upload

---
+++ week03_mouse/main.cpp
@@ -0,0 +1,26 @@
+#include<GL/glut.h>
+float mouseX=0,mouseY=0;
+void myTeapot(float x,float y)
+{
+    glPushMatrix();
+    glTranslatef(x,y,0);
+    glColor3f(1,1,0);
+    glutSolidTeapot(0.3);
+    glPopMatrix();
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    myTeapot((mouseX-150)/150.0,-(mouseY-150)/150.0);
+    glutSwapBuffers();
+}
+void mouse(int button,int state,int x,int y)
+{}
+int main(int argc,char**argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week03 ²¾°Ê");
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
